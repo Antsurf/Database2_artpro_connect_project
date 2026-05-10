86a2779bb8a6eed90a25d6667cf85126efa67d83
Antoine Richard <antoine.richard972@gmail.com>
2026-05-10 20:28:09 +0200

    Added ExhibitionService.java
    Needed for JdbcExhibitionService

---
+++ src/main/java/com/project/artconnect/service/ExhibitionService.java
@@ -0,0 +1,12 @@
+package com.project.artconnect.service;
+
+import com.project.artconnect.model.Exhibition;
+
+import java.util.List;
+
+public interface ExhibitionService {
+    public List<Exhibition> findAll();
+    public void save(Exhibition exhibition);
+    public void update(Exhibition exhibition);
+    public void delete(String title);
+}
