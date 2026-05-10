231ce4dcf178aa20d24d17eac460d460004f501a
Antoine Richard <antoine.richard972@gmail.com>
2026-05-10 20:15:26 +0200

    JdbcCommunityMemberService.java done

---
+++ src/main/java/com/project/artconnect/service/impl/JdbcCommunityMemberService.java
@@ -0,0 +1,40 @@
+package com.project.artconnect.service.impl;
+
+import com.project.artconnect.dao.CommunityMemberDao;
+import com.project.artconnect.model.CommunityMember;
+import com.project.artconnect.model.Review;
+import com.project.artconnect.model.Artwork;
+import com.project.artconnect.persistence.JdbcCommunityMemberDao;
+import com.project.artconnect.service.CommunityService;
+import com.project.artconnect.util.ConnectionManager;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+public class JdbcCommunityMemberService implements CommunityService {
+
+    private final CommunityMemberDao communityMemberDao = new JdbcCommunityMemberDao();
+
+    @Override
+    public List<CommunityMember> getAllMembers() {
+        return communityMemberDao.findAll();
+    }
+
+    // returns all the people that have same name
+    @Override
+    public Optional<CommunityMember> getMemberByName(String name) {
+        // same explanation as in JdbcArtworkService.java
+        return communityMemberDao.findAll().stream().filter(m->m.getName().equalsIgnoreCase(name)).findFirst();
+    }
+
+    // TODO : verify if it works after complete implementation of community member jdbc
+    @Override
+    public List<Review> getReviewsByMember(CommunityMember member) {
+        if (!member.getReviews().isEmpty()) {
+            return member.getReviews();
+        }
+        return List.of();
+
+    }
+}
