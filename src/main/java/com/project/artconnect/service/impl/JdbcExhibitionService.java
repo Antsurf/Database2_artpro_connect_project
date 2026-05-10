90bb3f6a03e1b0b80464fe8a3e3778de82d156f4
Antoine Richard <antoine.richard972@gmail.com>
2026-05-10 20:27:24 +0200

    JdbcExhibitionService.java fixed
    Was having the wrong cast (wrong interface and wrong variable definition)
    
    Fixed

--- src/main/java/com/project/artconnect/service/impl/JdbcExhibitionService.java
+++ src/main/java/com/project/artconnect/service/impl/JdbcExhibitionService.java
@@ -2,11 +2,13 @@ package com.project.artconnect.service.impl;
 
 import com.project.artconnect.dao.ExhibitionDao;
 import com.project.artconnect.model.Exhibition;
+import com.project.artconnect.persistence.JdbcExhibitionDao;
+import com.project.artconnect.service.ExhibitionService;
 
 import java.util.List;
 
-public class JdbcExhibitionService implements ExhibitionDao {
-    private final ExhibitionDao exhibitionDao = new JdbcExhibitionService();
+public class JdbcExhibitionService implements ExhibitionService {
+    private final ExhibitionDao exhibitionDao = new JdbcExhibitionDao();
     @Override
     public List<Exhibition> findAll() {
         return exhibitionDao.findAll();
